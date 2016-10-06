e08e845aa0a36995d5d1c22afc04a94aefa22453
Kerwen Zhang <xingchen0624@qq.com>
2016-10-06 22:07:53 +0800

    update

--- _posts/GetArticles.py
+++ _posts/GetArticles.py
@@ -5,14 +5,10 @@ import urllib.request
 import re
 
 
-html="http://cl.efqxi.com/thread0806.php?fid=7"
+firstPage="http://cl.efqxi.com/thread0806.php?fid=7"
 preURL="http://cl.efqxi.com/"
-#headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
-#req = urllib.request.Request(url=html, headers=headers)  
-#htmlSource = urllib.request.urlopen(req).read().decode('gbk')
-#soup = BeautifulSoup(htmlSource, "html.parser")
-
-
+pageHtml="http://cl.efqxi.com/thread0806.php?fid=7&search=&page="
+searchDepth=10
 
 def GetHTMLSoup(url):
     page = requests.get(url)
@@ -20,11 +16,24 @@ def GetHTMLSoup(url):
     soup = BeautifulSoup(page.text, "html.parser")
     return soup
 
-def Output(message):
+def Output(pre, url, element):
     try:
-        print(message)
+        f=open('C:\\Users\\Kerwen\\Desktop\\articles.txt','a')
+        f.write(str(pre))
+        f.write(str(url))
+        f.write('    ')
+        f.write(str(element))
+        f.write('\n')
     except:
-        print("error")
+        temp=10
+
+def OutputString(message):
+    try:
+        f=open('C:\\Users\\Kerwen\\Desktop\\articles.txt','a')
+        f.write(message)
+        f.write('\n')
+    except:
+        print("")
 
 def HasPictures(title):
     strTitle = str(title)
@@ -32,12 +41,17 @@ def HasPictures(title):
     if(strTitle.find('P]') >0 ):
         return True
     return False
-def FindArticlesWithPicture(element):
-    for j in element.find('a'):
-        if(HasPictures(j)):
-            parent = j.parent        
-            print(preURL,parent['href'], "   ",sep='',end=""),
-            Output(j)
+def FindArticlesWithPictureInSinglePage(html):
+    soup = GetHTMLSoup(html)
+    body = soup.body
+    for i in body.find_all('td', attrs={"style": "text-align:left;padding-left:8px"}):                     
+        for j in i.find('a'):
+            if(HasPictures(j)):
+                parent = j.parent        
+                Output(preURL, parent['href'], j)
+
+
+            
 def HasGoodArticles(element):
     lists = element.find_all('font')
     if(lists):
@@ -50,31 +64,57 @@ def HasGoodArticles(element):
 def FindGoodArticle(element):
     for j in element.find('a'):
         parent = j.parent        
-        print(preURL,parent['href'], "   ",sep='',end=""),
-        Output(j)
-def SearchArticle(html):
+        Output(preURL,parent['href'], j)
+        
+
+        
+def SearchGooArticleInSinglePage(html):
     soup = GetHTMLSoup(html)
     body = soup.body
     for i in body.find_all('td', attrs={"style": "text-align:left;padding-left:8px"}):  
-        #FindArticlesWithPicture(i)
         if(HasGoodArticles(i)):
              FindGoodArticle(i)
-# Main Entry
 
-pageHtml="http://cl.efqxi.com/thread0806.php?fid=7&search=&page="
-SearchArticle(html)
-for i in range(1,10):
-    SearchArticle(pageHtml+str(i))
+
+            
+def SearchWithKeyWord(KeyWord):
+    SearchWithKeyWordInSinglePage(KeyWord, firstPage)
+    for i in range(1,searchDepth):
+        SearchWithKeyWordInSinglePage(KeyWord, pageHtml+str(i))
+
+def SearchWithKeyWordInSinglePage(KeyWord, html):
+    soup = GetHTMLSoup(html)
+    body = soup.body
+    for i in body.find_all('td', attrs={"style": "text-align:left;padding-left:8px"}):
+        for j in i.find('a'):
+            title = str(j)
+            if (title.find(KeyWord)>0):
+                parent = j.parent
+                Output(preURL, parent['href'], j)
+
+
+def FindArticlesWithPictures():
+    FindArticlesWithPictureInSinglePage(firstPage)
+    for i in range(1, searchDepth):
+          FindArticlesWithPictureInSinglePage(pageHtml+str(i))
+
+def SearchGoodArticles():
+    SearchGooArticleInSinglePage(firstPage)
+    for i in range(1,searchDepth):
+        SearchGooArticleInSinglePage(pageHtml+str(i))
+
+def SearchWithKeyWords(keys):
+    list=keys.split(' ')
+    if list:
+        for KeyWord in list:
+            OutputString(KeyWord)
+            SearchWithKeyWord(KeyWord)
+
+# Main Entry
+#SearchWithKeyWords("求助")
+#FindArticlesWithPictures()
+SearchGoodArticles()
 
     
 
-#page = requests.get(html)
-#print(page.encoding, page.apparent_encoding)
-#page.encoding = page.apparent_encoding
 
-#soup = BeautifulSoup(page.text, "html.parser")
-#try:
-#    print(soup.prettify())
-#except Exception as err:
-#    print(err)
-#http://cl.efqxi.com/htm_data/7/1610/2091884.html
